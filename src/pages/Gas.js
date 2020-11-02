714d0feda067ccf5c0281c500ce79acce560877d
menagetdev <info@menaget.com>
2020-11-02 10:40:50 +0100

    Connections

--- src/pages/Gas.js
+++ src/pages/Gas.js
@@ -162,51 +162,6 @@ class Gas extends Component {
                                                 </div>
 
                                                 {/*<div*/}
-                                                {/*    className="col-sm-12 col-md-3"*/}
-                                                {/*    style={{zIndex: 10}}>*/}
-                                                {/*    <div className="form-group gas-option-wrapper">*/}
-                                                  {/*        <input id="gas-12.5"*/}
-                                                {/*               type="radio"*/}
-                                                {/*               name="size"*/}
-                                                {/*               onChange={handleChange}*/}
-                                                {/*               value="12.5KG"*/}
-                                                {/*               data-size="12.5KG"*/}
-                                                {/*               data-amount="4500"*/}
-                                                {/*               className="check-image"/>*/}
-                                                {/*        <label*/}
-                                                {/*            htmlFor="gas-12.5">*/}
-                                                {/*            <img*/}
-                                                {/*                src="/images/products/12v.png"*/}
-                                                {/*                alt=""/>*/}
-                                                {/*            <div*/}
-                                                {/*                className="label-check_text-meta">12.5KG <span*/}
-                                                {/*                className="price">₦4,500</span>*/}
-                                                {/*            </div>*/}
-                                                {/*        </label>*/}
-                                                {/*        /!*<label*!/*/}
-                                                {/*        /!*    htmlFor="gas-cylinder"*!/*/}
-                                                {/*        /!*    className="label-check_text">*!/*/}
-                                                {/*        /!*    <img*!/*/}
-                                                {/*        /!*        src="/images/products/12v2.png"*!/*/}
-                                                {/*        /!*        alt=""/>*!/*/}
-                                                {/*        /!*    <input*!/*/}
-                                                {/*        /!*        id="gas-cylinder"*!/*/}
-                                                {/*        /!*        type="radio"*!/*/}
-                                                {/*        /!*        name="size" onChange={handleChange}*!/*/}
-                                                {/*        /!*        value="12.5KG + Cylinder"*!/*/}
-                                                {/*        /!*        data-size="12.5KG+cylinder"*!/*/}
-                                                {/*        /!*        data-amount="15750"*!/*/}
-                                                {/*        /!*        className="check-image"/>*!/*/}
-                                                {/*        /!*    <div className="label-check_text-meta">*!/*/}
-                                                {/*        /!*        <span className="label-check_text-descr">Purchase 12.5kg Ressortir branded Gas Cylinder </span>*!/*/}
-                                                {/*        /!*        <span className="price">₦15,750</span>*!/*/}
-                                                {/*        /!*    </div>*!/*/}
-                                                {/*        /!*</label>*!/*/}
-                                                {/*    </div>*/}
-                                                {/*</div>*/}
-
-
-                                                {/*<div*/}
                                                 {/*    className="col-sm-12 col-md-3">*/}
                                                 {/*    <div*/}
                                                 {/*        className="form-group gas-option-wrapper">*/}
